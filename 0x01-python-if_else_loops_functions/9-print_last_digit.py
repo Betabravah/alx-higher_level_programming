61bfea3afacba4f9440a72d360a61a21157ce8f7
Betabravah <betabravah@gmail.com>
2022-06-29 00:55:14 +0300

    python

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+def print_last_digit(number):
+    if number > 0:
+        last_digit = number % 10
+    else:
+        last_digit = abs(number) % 10
+    print(last_digit, end="")
